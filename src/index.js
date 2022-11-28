c89d09bc8e77a29711231a64154d8dcf856ff744
Jagadish Shrestha <shresthadev403@gmail.com>
2022-11-28 15:22:23 +0545

    git hub pages added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   
