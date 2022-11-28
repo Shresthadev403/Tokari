0e91250231e1e17a9d359522a004ec8d3edfab21
Jagadish Shrestha <shresthadev403@gmail.com>
2022-11-28 13:11:29 +0545

    router fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   
